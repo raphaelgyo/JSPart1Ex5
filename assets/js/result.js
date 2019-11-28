f058c4340018dddee9bf90acf813d32ec7b7b813
raphaelgyo <raphael.granizo@gmail.com>
2019-11-28 15:30:37 +0100

    correction

--- assets/js/result.js
+++ assets/js/result.js
@@ -1,1 +1,19 @@
-
+var validation = document.getElementById('submit');
+validation.addEventListener('click', f_valid);
+function f_valid() {
+  // Déclaration des variables qui vont récupérer les valeurs des champs de saisie
+  var firstNumber = document.getElementById('firstNumber').value;
+  var secondNumber = document.getElementById('secondNumber').value;
+  if (isNaN(firstNumber) == true || isNaN(secondNumber)){
+    alert('erreur : ce n\'est pas un nombre');
+  }else{
+    if (secondNumber != 0) {
+      /* Déclaration d'une variable result qui contient le reste (modulo) de la division du nombre1 par le nombre2 */
+      var result = firstNumber % secondNumber;
+      //La fonction Math.round() retourne la valeur d'un nombre arrondi à l'entier le plus proche.
+      alert(Math.round(result));
+    }else{
+      alert('erreur : car division par 0');
+    }
+  }
+}
